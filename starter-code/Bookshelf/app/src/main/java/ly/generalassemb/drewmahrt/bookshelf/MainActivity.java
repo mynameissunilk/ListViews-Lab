cee823f53ecf730c03c4959f5bd873c2c3d79ee2
Sunil <mynameis.sunilk@gmail.com>
2016-07-04 17:33:50 -0400

    used strings.xml and String.format() to setText in a different way

--- starter-code/Bookshelf/app/src/main/java/ly/generalassemb/drewmahrt/bookshelf/MainActivity.java
+++ starter-code/Bookshelf/app/src/main/java/ly/generalassemb/drewmahrt/bookshelf/MainActivity.java
@@ -1,6 +1,7 @@
 package ly.generalassemb.drewmahrt.bookshelf;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,8 +21,8 @@ public class MainActivity extends AppCompatActivity {
     BaseAdapter mAdapter;
     ListView lView;
     List<Book> mBookList = new ArrayList<>();
-
-
+    //String mAuthor = "Author ";
+    //String mTitle = "Title ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         //Below is a partially complete example for one Adapter
+        //anonymous inner class of abstract class BaseAdapter.
         mAdapter = new BaseAdapter() {
             @Override
             public int getCount() {
@@ -55,14 +57,21 @@ public class MainActivity extends AppCompatActivity {
             public View getView(int position, View convertView, ViewGroup parent) {
                 View vInflate = convertView;
                 if(vInflate == null){
-                    LayoutInflater inflater = (LayoutInflater) MainActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                    LayoutInflater inflater = (LayoutInflater) MainActivity
+                            .this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
                     vInflate = inflater.inflate(android.R.layout.simple_list_item_1, null);
                 }
 
                 TextView tv = (TextView) vInflate.findViewById(android.R.id.text1);
-                tv.setText("Title:  " + mBookList.get(position).getTitle() + "\nAuthor:  " + mBookList.get(position).getAuthor());
 
-
+                //tv.setText("Title:  " + mBookList.get(position).getTitle() + "\nAuthor:  " + mBookList.get(position).getAuthor());
+                //tv.setText(mTitle + mBookList.get(position).getTitle() + "\n"+mAuthor + mBookList.get(position).getAuthor());
+                //Above two work, trying with strings.xml * ids.
+                //tv.setText(R.string.mTitle + mBookList.get(position).getAuthor() +"\n" + R.string.mAuthor + mBookList.get(position).getAuthor());
+                Resources res = getResources();
+                String text = String.format(res.getString(R.string.printout), mBookList.get(position).getTitle(), mBookList.get(position).getAuthor());
+                tv.setText(text);
 
                 //TODO: Update the view
                 return vInflate;
